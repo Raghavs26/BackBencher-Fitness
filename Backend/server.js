82177510a72c76db765d3c5b3076f0873e5b6eaa
Raghav <raghavsekhri2k1@gmail.com>
2022-05-10 13:44:37 +0530

    second commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
-require('./db/conn')
-const register=require('./models/register')
+require("./db/conn");
+const register = require("./route/userRoute");
 
-app.get('/',(req,res)=>{
-    res.send("Server started");
-})
-app.use(express.json())
-app.use('/api/users/',register)
+app.get("/", (req, res) => {
+  res.send("Server started");
+});
+app.use(express.json());
+app.use("/api/users/", register);
 
 app.listen(port, () => {
   console.log("port on 5000");
